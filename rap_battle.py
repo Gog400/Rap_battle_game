3b620a3ab2b632e6a9ad371c95458d17fdb7d61e
Gog400 <gog400@mail.ru>
2019-03-10 13:50:41 +0300

    Punchline test

--- rap_battle.py
+++ rap_battle.py
@@ -13,9 +13,43 @@ NewLamborgini = {
     'name': 'NewLamborgini',
     'bars': 'Новый ламборгини, новые цацки. Моя жизнь - это Каноха, ты в ней Акацке'
 }
+punchline_1 = {
+    'id': 1,
+    'bars': 'Я сосу бибу'
+    'theme': 'gangsta'
+}
+punchline_2 = {
+    'id': 2,
+    'bars': 'Я сосу бибу'
+    'theme': 'million'
+}
+punchline_3 = {
+    'id': 3,
+    'bars': 'Я сосу бибу'
+    'theme': 'minecraft'
+}
+punchline_4 = {
+    'id': 4,
+    'bars': 'Я сосу бибу'
+    'theme': 'elephant'
+}
+
+name = input('Введите свой реперский ник: ')
+print('''
+1. Одинокий Гангстер
+2. Миллионер из трущоб
+3. Любитель игры в Майнкрафт
+4. Ценитель К/Ф Зелёный Слоник
+''')
+
+background = input('Выберите своё прошлое: ')
+if background == 1:
+    urRapper['bars'].append()
+
 ##Описание основного персонажа
 urRapper = {
-    'inv': []
+    'inv': [],
+    'bars': []
 }
 
 ##Добавление(лут) айтемов в инвентарь
