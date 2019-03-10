561c8ac6fbeb425696552f52fa0e148a79db06f2
Gog400 <gog400@mail.ru>
2019-03-10 15:11:25 +0300

    Bug_2(fixed)

---
+++ rb.py
@@ -0,0 +1,14 @@
+import random
+
+urRaper = {
+    'inv': [],
+    'bars': [],
+    'class': 'none'
+}
+
+print("1. Одинокий Гангстер")
+print("2. Миллионер из трущоб")
+print("3. Любитель игры в Майнкрафт")
+print("4. Ценитель Х/Ф Зелёный Слоник")
+
+background = int(input)
